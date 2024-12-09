81bf4bf9e22f4cac51183ecd40b2cb781b6ce187
helenatai <115176130+helenatai@users.noreply.github.com>
2024-12-09 13:57:04 +0000

    initial commit

---
+++ src/components/authentication/AuthWrapper1.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+// material-ui
+import { styled } from '@mui/material/styles';
+
+// types
+import { ThemeMode } from 'types/config';
+
+// ==============================|| AUTHENTICATION 1 WRAPPER ||============================== //
+
+const AuthWrapper1 = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.mode === ThemeMode.DARK ? theme.palette.background.default : theme.palette.grey[100],
+  minHeight: '100vh',
+  backgroundImage: 'url(assets/images/auth/home-study-here.jpg)', // Add your image path here
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+}));
+
+export default AuthWrapper1;
---
+++ src/menu-items/index.tsx
@@ -0,0 +1,14 @@
+// menu import
+import samplePage from './sample-page';
+import pages from './pages';
+
+// types
+import { NavItemType } from 'types';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems: { items: NavItemType[] } = {
+  items: [samplePage, pages]
+};
+
+export default menuItems;
