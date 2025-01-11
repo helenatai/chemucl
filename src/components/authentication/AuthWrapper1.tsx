76de1691ffec742f670a9458a2639317f5e8253d
helenatai <115176130+helenatai@users.noreply.github.com>
2025-01-11 15:58:23 +0000

    Initial commit

--- src/components/authentication/AuthWrapper1.tsx
+++ src/components/authentication/AuthWrapper1.tsx
@@ -11,7 +11,7 @@ import { ThemeMode } from 'types/config';
 const AuthWrapper1 = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.mode === ThemeMode.DARK ? theme.palette.background.default : theme.palette.grey[100],
   minHeight: '100vh',
-  backgroundImage: 'url(assets/images/auth/home-study-here.jpg)', // Add your image path here
+  backgroundImage: 'url(assets/images/auth/home-study-here.jpg)', 
   backgroundSize: 'cover',
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
--- src/components/authentication/auth-forms/AuthLogin.tsx
+++ src/components/authentication/auth-forms/AuthLogin.tsx
@@ -16,6 +16,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 // third party
 import * as Yup from 'yup';
@@ -51,118 +52,147 @@ const JWTLogin = ({ loginProp, ...others }: { loginProp?: number }) => {
   };
 
   return (
-    
-    <Formik
-      initialValues={{
-        email: 'info@codedthemes.com',
-        password: '123456',
-        submit: null
-      }}
-      validationSchema={Yup.object().shape({
-        email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-        password: Yup.string().max(255).required('Password is required')
-      })}
-      onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-        try {
-          await login(values.email, values.password);
-          if (scriptedRef.current) {
-            setStatus({ success: true });
-            router.push(DASHBOARD_PATH);
-            setSubmitting(false);
-          }
-        } catch (err: any) {
-          console.error(err);
-          if (scriptedRef.current) {
-            setStatus({ success: false });
-            setErrors({ submit: err.message });
-            setSubmitting(false);
-          }
-        }
-      }}
-    >
-      {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
-        <form noValidate onSubmit={handleSubmit} {...others}>
-          <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
-            <InputLabel htmlFor="outlined-adornment-email-login">Email Address / Username</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-email-login"
-              type="email"
-              value={values.email}
-              name="email"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              inputProps={{}}
-            />
-            {touched.email && errors.email && (
-              <FormHelperText error id="standard-weight-helper-text-email-login">
-                {errors.email}
-              </FormHelperText>
-            )}
-          </FormControl>
-
-          <FormControl fullWidth error={Boolean(touched.password && errors.password)} sx={{ ...theme.typography.customInput }}>
-            <InputLabel htmlFor="outlined-adornment-password-login">Password</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password-login"
-              type={showPassword ? 'text' : 'password'}
-              value={values.password}
-              name="password"
-              onBlur={handleBlur}
-              onChange={handleChange}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                    size="large"
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              inputProps={{}}
-              label="Password"
-            />
-            {touched.password && errors.password && (
-              <FormHelperText error id="standard-weight-helper-text-password-login">
-                {errors.password}
-              </FormHelperText>
-            )}
-          </FormControl>
-
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <FormControlLabel
-                control={
-                  <Checkbox checked={checked} onChange={(event) => setChecked(event.target.checked)} name="checked" color="primary" />
+    <Box>
+      <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Button
+              fullWidth
+              size="large"
+              variant="contained"
+              color="primary"
+              onClick={async () => {
+                const res = await signIn('uclapi', {
+                  redirect: false,
+                  callbackUrl: '/inventory-page'
+                });
+                if (res?.error) {
+                  alert(res.error);
                 }
-                label="Keep me logged in"
-              />
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle1" component={Link} href={'/forgot-password'} color="secondary" sx={{ textDecoration: 'none' }}>
-                Forgot Password?
-              </Typography>
-            </Grid>
+              }}
+            >
+              Sign in with UCL SSO
+            </Button>
           </Grid>
 
-          {errors.submit && (
-            <Box sx={{ mt: 3 }}>
-              <FormHelperText error>{errors.submit}</FormHelperText>
-            </Box>
-          )}
-          <Box sx={{ mt: 2 }}>
-            <AnimateButton>
-              <Button color="secondary" disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained">
-                Sign In
-              </Button>
-            </AnimateButton>
-          </Box>
-        </form>
-      )}
-    </Formik>
+          <Grid item xs={12}>
+            <Divider>or</Divider>
+          </Grid>
+          
+          <Grid item xs={12}>
+            <Formik
+              initialValues={{
+                email: 'info@codedthemes.com',
+                password: '123456',
+                submit: null
+              }}
+              validationSchema={Yup.object().shape({
+                email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
+                password: Yup.string().max(255).required('Password is required')
+              })}
+              onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+                try {
+                  await login(values.email, values.password);
+                  if (scriptedRef.current) {
+                    setStatus({ success: true });
+                    router.push(DASHBOARD_PATH);
+                    setSubmitting(false);
+                  }
+                } catch (err: any) {
+                  console.error(err);
+                  if (scriptedRef.current) {
+                    setStatus({ success: false });
+                    setErrors({ submit: err.message });
+                    setSubmitting(false);
+                  }
+                }
+              }}
+            >
+              {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+                <form noValidate onSubmit={handleSubmit} {...others}>
+                  <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
+                    <InputLabel htmlFor="outlined-adornment-email-login">Email Address / Username</InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-email-login"
+                      type="email"
+                      value={values.email}
+                      name="email"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      inputProps={{}}
+                    />
+                    {touched.email && errors.email && (
+                      <FormHelperText error id="standard-weight-helper-text-email-login">
+                        {errors.email}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+
+                  <FormControl fullWidth error={Boolean(touched.password && errors.password)} sx={{ ...theme.typography.customInput }}>
+                    <InputLabel htmlFor="outlined-adornment-password-login">Password</InputLabel>
+                    <OutlinedInput
+                      id="outlined-adornment-password-login"
+                      type={showPassword ? 'text' : 'password'}
+                      value={values.password}
+                      name="password"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            size="large"
+                          >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      inputProps={{}}
+                      label="Password"
+                    />
+                    {touched.password && errors.password && (
+                      <FormHelperText error id="standard-weight-helper-text-password-login">
+                        {errors.password}
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+
+                  <Grid container alignItems="center" justifyContent="space-between">
+                    <Grid item>
+                      <FormControlLabel
+                        control={
+                          <Checkbox checked={checked} onChange={(event) => setChecked(event.target.checked)} name="checked" color="primary" />
+                        }
+                        label="Keep me logged in"
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="subtitle1" component={Link} href={'/forgot-password'} color="secondary" sx={{ textDecoration: 'none' }}>
+                        Forgot Password?
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  {errors.submit && (
+                    <Box sx={{ mt: 3 }}>
+                      <FormHelperText error>{errors.submit}</FormHelperText>
+                    </Box>
+                  )}
+                  <Box sx={{ mt: 2 }}>
+                    <AnimateButton>
+                      <Button color="secondary" disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained">
+                        Sign In
+                      </Button>
+                    </AnimateButton>
+                  </Box>
+                </form>
+              )}
+            </Formik>
+          </Grid>
+      </Grid>
+    </Box>
   );
 };
 
